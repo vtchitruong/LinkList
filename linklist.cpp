cd7d44873a86cbc0a8de51016f5039cc817ba8be
Chi Truong <vtchitruong@gmail.com>
2021-07-09 13:59:16 +0700

    print list

--- linklist.cpp
+++ linklist.cpp
@@ -2,51 +2,45 @@
 
 using namespace std;
 
-class SinglyLinkedListNode {
-    public:
-        int data;
-        SinglyLinkedListNode *next;
+// class of Node
+class SinglyLinkedListNode
+{
+public:
+    int data;
+    SinglyLinkedListNode *next;
 
-        SinglyLinkedListNode(int node_data) {
-            this->data = node_data;
-            this->next = nullptr;
-        }
-};
-
-class SinglyLinkedList {
-    public:
-        SinglyLinkedListNode *head;
-        SinglyLinkedListNode *tail;
-
-        SinglyLinkedList() {
-            this->head = nullptr;
-            this->tail = nullptr;
-        }
-
-        void insert_node(int node_data) {
-            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
-
-            if (!this->head) {
-                this->head = node;
-            } else {
-                this->tail->next = node;
-            }
-
-            this->tail = node;
-        }
-};
-
-void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
-    while (node) {
-        fout << node->data;
-
-        node = node->next;
-
-        if (node) {
-            fout << sep;
-        }
+    SinglyLinkedListNode(int node_data)
+    {
+        this->data = node_data;
+        this->next = nullptr;
     }
-}
+};
+
+//--------------------------------------------------------
+// class of List
+class SinglyLinkedList
+{
+public:
+    SinglyLinkedListNode *head;
+    SinglyLinkedListNode *tail;
+
+    SinglyLinkedList() {
+        this->head = nullptr;
+        this->tail = nullptr;
+    }
+
+    void insert_node(int node_data) {
+        SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+        if (!this->head) {
+            this->head = node;
+        } else {
+            this->tail->next = node;
+        }
+
+        this->tail = node;
+    }
+};
 
 void free_singly_linked_list(SinglyLinkedListNode* node) {
     while (node) {
@@ -57,17 +51,18 @@ void free_singly_linked_list(SinglyLinkedListNode* node) {
     }
 }
 
-// Complete the compare_lists function below.
+void printLinkedList(SinglyLinkedListNode* head)
+{
+    cout << "\nOutput the singly linked list:\n";
+    for (SinglyLinkedListNode *p = head; p; p = p->next)
+    {
+        cout << p->data << "->";
+    }
+}
 
-/*
- * For your reference:
- *
- * SinglyLinkedListNode {
- *     int data;
- *     SinglyLinkedListNode* next;
- * };
- *
- */
+
+
+
 bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
     bool res = true;
     
@@ -87,47 +82,21 @@ bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    SinglyLinkedList* llist = new SinglyLinkedList();
 
-    int tests;
-    cin >> tests;
+    int llist_count;
+    cin >> llist_count;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    for (int tests_itr = 0; tests_itr < tests; tests_itr++) {
-        SinglyLinkedList* llist1 = new SinglyLinkedList();
-
-        int llist1_count;
-        cin >> llist1_count;
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-        for (int i = 0; i < llist1_count; i++) {
-            int llist1_item;
-            cin >> llist1_item;
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-            llist1->insert_node(llist1_item);
-        }
-      
-      	SinglyLinkedList* llist2 = new SinglyLinkedList();
-
-        int llist2_count;
-        cin >> llist2_count;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-        for (int i = 0; i < llist2_count; i++) {
-            int llist2_item;
-            cin >> llist2_item;
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-            llist2->insert_node(llist2_item);
-        }
-
-        bool result = compare_lists(llist1->head, llist2->head);
-
-        fout << result << "\n";
+        llist->insert_node(llist_item);
     }
 
-    fout.close();
+    printLinkedList(llist->head);
 
     return 0;
 }
